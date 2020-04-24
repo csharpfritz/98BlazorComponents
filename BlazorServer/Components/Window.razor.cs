10bb935ab92b4894f884589458a1d6c53345b8c1
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-04-24 12:04:28 -0400

    MVP of Windows 98 Blazor components

---
+++ BlazorServer/Components/Window.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Components
+{
+	public partial class Window
+	{
+
+		//[Parameter]
+		//public int Width { get; set; }
+
+		//[Parameter]
+		//public string Title { get; set; }
+
+
+	}
+}
